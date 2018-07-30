ce7ca2c77f7a1d6fca3afa07957d13f9a209dd9c
alessandrofuda <alessandro.fuda@gmail.com>
2018-07-30 16:34:50 +0000

    API implementation: add custom json response to 404 instead of standard stack exception page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -43,7 +43,20 @@ class Handler extends ExceptionHandler
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $exception)
-    {
+    {   
+
+        
+        // This will replace our 404 response with
+        // a JSON response.
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json([
+                'error' => 'Resource not found'
+            ], 404);
+        }
+
+
+
+
         return parent::render($request, $exception);
     }
 
